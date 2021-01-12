a28c5261656b457fb88c6fa3aee4a27dd44ce8b8
purplerocwu <purplerocwu@tencent.com>
2021-01-12 13:58:06 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ export GOPROXY=direct
 sudo apt-get update
 sudo apt-get install gcc-mingw-w64-i686 gcc-multilib
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./linux_amd64_PBEBrute ./PBEBrute/
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./linux_amd64_PBEBrute ./
 tar -czvf linux_amd64_PBEBrute.tar.gz linux_amd64_PBEBrute
 
-CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./linux_386_PBEBrute ./PBEBrute/
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./linux_386_PBEBrute ./
 tar -czvf linux_386_PBEBrute.tar.gz linux_386_PBEBrute
 
-CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./windows_386_PBEBrute ./PBEBrute/
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./windows_386_PBEBrute ./
 tar -czvf windows_386_PBEBrute.tar.gz windows_386_PBEBrute
 
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./windows_amd64_PBEBrute ./PBEBrute/
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./windows_amd64_PBEBrute ./
 tar -czvf windows_amd64_PBEBrute.tar.gz windows_amd64_PBEBrute
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./darwin_amd64_PBEBrute ./PBEBrute/
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -extldflags -static -extldflags -static" -o ./darwin_amd64_PBEBrute ./
 tar -czvf darwin_amd64_PBEBrute.tar.gz darwin_amd64_PBEBrute
